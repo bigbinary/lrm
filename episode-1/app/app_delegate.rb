f67d17633fd642b8b1422b5320e1771876223cd3
rubyrajesh <teja.thummalapally@gmail.com>
2013-11-25 15:28:04 -0500

    Setting languages controller as the root view controller and not using UINavigation controller

--- episode-1/app/app_delegate.rb
+++ episode-1/app/app_delegate.rb
@@ -3,7 +3,7 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
     @languages_controller = LanguagesController.alloc.initWithNibName(nil, bundle:nil)
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(@languages_controller)
+    @window.rootViewController = @languages_controller
 
     @window.makeKeyAndVisible
     true
