df80a0acaccb2b685e53a53d0a1ead8fea20a45f
Neeraj Singh <neerajdotname@gmail.com>
2013-12-01 19:52:45 -0500

    wip

--- episode-xx/app/login_controller.rb
+++ episode-xx/app/login_controller.rb
@@ -3,10 +3,24 @@ class LoginController < UIViewController
   def viewDidLoad
     super
 
-    rmq.append(UIView, :form).tap do |q|
-      @email = q.append(UITextField, :email).get
-      @password = q.append(UITextField, :password).get
+    rmq.append(UIView, :login_form).tap do |q| 
+      email = q.append(UITextField, :email).get
+      pwd = q.append(UITextField, :password).get
+          
+      rmq(email).style do |st|
+        st.frame = {l: 100, t: 80, w: 100, h: 30} 
+        st.background_color = rmq.color.white
+      end 
+      rmq(pwd).style do |st|
+        st.frame = {l: 100, t: 120, w: 100, h: 30} 
+        st.background_color = rmq.color.white
+      end 
+    end 
+    rmq(:login_form).style do |st|
+      st.frame = {l: 20, t: 30, w:200, h: 250}
+      st.background_color = rmq.color.white
     end
+
 
   end
 
